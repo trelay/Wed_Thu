68cf5315977057df0603f4e51b6c0888ad6a379d
Trelay wang <trelwan@celestica.com>
2019-03-03 22:02:43 +0800

    The k line of Gree from 2016.9 to 2019.3

--- Buy_sell.py
+++ Buy_sell.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python2
 #coding=utf-8
-import requests
-import demjson
-from pandas import Series
-import pandas as pd
-from pandas import DataFrame
+import matplotlib.pyplot as plt
 import tushare as ts
-import numpy
+from pandas import DataFrame
+import pandas as pd
+import numpy as np
+
+df = ts.get_hist_data('000651')
+gree_test = DataFrame(df, columns = ["open", "close", "high"])
+
+fig = plt.figure()
+
+data_2019=gree_test["close"].sort_index(ascending=True)
+data_2019.cumsum()
+data_2019.plot()
+plt.show()
