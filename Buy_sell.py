b2105611ae3469e3ebccba5e5c971c1fb93a6b2e
Trelay wang <trelwan@celestica.com>
2019-03-03 11:34:00 +0800

    modify the file

--- Buy_sell.py
+++ Buy_sell.py
@@ -1,2 +1,9 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python2
+#coding=utf-8
+import requests
+import demjson
+from pandas import Series
+import pandas as pd
+from pandas import DataFrame
+import tushare as ts
+import numpy
