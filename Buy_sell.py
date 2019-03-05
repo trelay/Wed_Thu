167819649fd9f692f5c5de38dd4e794150aefd13
Trelay wang <trelwan@celestica.com>
2019-03-05 22:46:08 +0800

    Finish mode creating for buy and sell, not success.

--- Buy_sell.py
+++ Buy_sell.py
@@ -19,62 +19,50 @@ df = ts.get_hist_data('000651')
 
 money = 50000
 shares_own = 10
-shares_total = shares_own
-
-action = 0
-rate = 0
-p_Base= 0
 rate_up = 0.7
 rate_down = 0.5
 
 p_yesterday_close= 0
 
+price_last=()
 
-
-def op_shares(action,rate,p_yesterday_close, popen):
+def op_shares(price_last, popen_today):
     global money
     global shares_own
-    global shares_total
+    price_move = abs(price_last[1]-price_last[3])
 
     shares_act = 0
-    if action == 1 and popen > (p_yesterday_close*(1+ rate* rate_up)):
-        print("Buying")
+    #if price_last[3] > (popen_today + price_move*rate_up):
+    if price_last[3] < (popen_today - price_move*rate_down):
+        print("Buying with price: {}".format(popen_today))
         shares_act = math.floor(money/(popen*100))
-        shares_total = shares_own + shares_act
+        shares_own = shares_own + shares_act
+
         money = money - shares_act * popen * 100
+        print("I have totally share(*100):{}".format(shares_own))
         
-    elif action == -1 and popen < (p_yesterday_close*(1- rate* rate_down)):
-        print("sell")
-        shares_act = math.floor(money/(popen*100))
-        shares_total = shares_own - shares_act
+    #elif price_last[3] < (popen_today - price_move*rate_down):
+    elif price_last[3] > (popen_today + price_move*rate_up):
+        print("Selling with price: {}".format(popen_today))
+        shares_act = shares_own
+        shares_own = shares_own - shares_act
         money = money + shares_act * popen * 100
+        print("I have total money:{}".format(money))
 
 data = df.sort_index(ascending=True)
 t=0
 for date in data.index:
-    #print(date)
     data_day_df = DataFrame(data, index=[date])
     popen = DataFrame(data_day_df.T, index = ["open"]) .values[0][0]
     pclose = DataFrame(data_day_df.T, index = ["close"]) .values[0][0] 
     phigh = DataFrame(data_day_df.T, index = ["high"]) .values[0][0] 
     plow = DataFrame(data_day_df.T, index = ["low"]) .values[0][0]
-
     if t>0:
-        op_shares(action,rate,p_yesterday_close,popen)
+        op_shares(price_last, popen)
 
-    if phigh == pclose:
-        action = 0
-        rate = 0
-    elif phigh > pclose:
-        action = 1
-        rate = (phigh - pclose)/pclose
-    elif phigh < pclose:
-        action == -1
-        rate = (pclose - phigh)/pclose
-
-    p_yesterday_close = pclose
+    price_last = (popen,phigh,plow,pclose)
 
     t+=1
 
 print('The money I have: {0}'.format(money))
-print('The share I have: {0}'.format(shares_total))
+print('The share I have: {0}'.format(shares_own))
