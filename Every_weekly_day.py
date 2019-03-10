f77f6945e7aad69ae3dce5433ee16dfa98dd24bd
Trelay wang <trelwan@celestica.com>
2019-03-10 21:13:00 +0800

    Add the result of this theory.

--- Every_weekly_day.py
+++ Every_weekly_day.py
@@ -13,7 +13,7 @@ df["Date"] = df.index
 
 df['Date'] = pd.to_datetime(df['Date'])
 df['Week_Number'] = df['Date'].dt.dayofweek
-del df["Date"]
+
 
 is_0 = df["Week_Number"] ==0
 df_0 = df[is_0]
@@ -30,9 +30,43 @@ df_3 = df[is_3]
 is_4 = df["Week_Number"] ==4
 df_4 = df[is_4]
 
-print("Let's compete:")
+print("Let's compete the pprice(%) of dayofweek:")
 print("Day_1: {}".format(df_0["p_change"].sum()))
 print("Day_2: {}".format(df_1["p_change"].sum()))
 print("Day_3: {}".format(df_2["p_change"].sum()))
 print("Day_4: {}".format(df_3["p_change"].sum()))
 print("Day_5: {}".format(df_4["p_change"].sum()))
+
+money = 50000
+shares_own = 10
+
+def sell_out(p_trade):
+    global money
+    global shares_own
+    shares_act = shares_own
+    shares_own = shares_own - shares_act
+    money = money + shares_act * p_trade * 100
+
+def buy_in(p_trade):
+    global money
+    global shares_own
+    shares_act = math.floor(money/(p_trade*100))
+    shares_own = shares_own + shares_act
+    money = money - shares_act * p_trade * 100
+
+data = df.sort_index(ascending=True)
+for date in data.index:
+    data_day_df = DataFrame(data, index=[date])
+    Week_Number = DataFrame(data_day_df.T, index = ["Week_Number"]) .values[0][0]
+    phigh = DataFrame(data_day_df.T, index = ["high"]) .values[0][0] 
+    plow = DataFrame(data_day_df.T, index = ["low"]) .values[0][0]
+    pclose = DataFrame(data_day_df.T, index = ["close"]) .values[0][0] 
+    p_trade = (plow+ phigh)/2
+
+    if int(Week_Number) == 1:
+        sell_out(pclose)
+    elif int(Week_Number) ==2:
+        buy_in(pclose)
+
+print('The money I have: {0}'.format(money))
+print('The share I have: {0}'.format(shares_own))
